024c2ca0f24292eeac5dff383b78aa01e55d47ca
Duong1111111 <nguyenvanduong151000@gmail.com>
2025-12-24 18:29:36 +0700

    Thêm trường tasktype

--- cruds/task.py
+++ cruds/task.py
@@ -76,6 +76,7 @@ def create_task(db: Session, task_in: TaskCreate, current_user: User):
         deadline=task_in.deadline,
         status=task_in.status,
         priority=task_in.priority,
+        task_type=task_in.task_type,
         assignee_id=task_in.assignee_id,
         reviewer_id=task_in.reviewer_id,
         source_type=task_in.source_type
