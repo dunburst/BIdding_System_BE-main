937bc7f17ef98e007f0674e228023a42e8db7f30
Duong1111111 <nguyenduong151000@gmail.com>
2026-01-14 08:20:26 +0700

    first commit

--- main.py
+++ main.py
@@ -111,4 +111,4 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     )
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=43210)
